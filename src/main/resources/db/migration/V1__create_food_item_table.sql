a1b6e8c06995620a6fd53d667550713ed1f50064
Felipe <camargo963@gmail.com>
2025-11-13 00:53:21 -0300

    feat: Cria a migration da tabela food_item

---
+++ src/main/resources/db/migration/V1__create_food_item_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE food_item (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    categoria ENUM('CARNE', 'LATICINIOS', 'VEGETAIS') NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    quantidade INT NOT NULL,
+    validade DATE NOT NULL
+);
